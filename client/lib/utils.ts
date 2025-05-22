6d98babe93b1f6575ada42725e7b6b9b518c4cd0
cuongthieu-itme <thieutrancuong.me@gmail.com>
2025-05-22 16:39:07 +0700

    update

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const formatDate = (date: Date): string => {
   return new Intl.DateTimeFormat('vi-VN', {
     year: 'numeric',
@@ -10,4 +17,31 @@ export const truncateString = (str: string, length: number): string => {
   if (!str) return '';
   if (str.length <= length) return str;
   return str.substring(0, length) + '...';
+};
+
+/**
+ * Sanitizes user input by removing unwanted characters
+ * and trimming whitespace
+ *
+ * @param input - The string to sanitize
+ * @returns Sanitized string
+ */
+export const sanitizeInput = (input: string): string => {
+  if (!input) return '';
+
+  // Trim whitespace from both ends
+  let sanitized = input.trim();
+
+  // Remove any script tags and other potentially dangerous content
+  sanitized = sanitized.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+
+  // Convert HTML entities
+  sanitized = sanitized
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#x27;');
+
+  return sanitized;
 };
